e6fea956178a8f23e6aca049227bf7a7289d7c29
Gabriel B. Di Rezze <bortoletigabriel.drz@gmail.com>
2025-07-13 12:01:40 -0300

    feat(devices): device page base layout

--- lib/screens/devices/devices_page.dart
+++ lib/screens/devices/devices_page.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_house_app/theme/app_colors.dart';
+import 'package:smart_house_app/widgets/auth_input.dart';
+import 'package:smart_house_app/widgets/device_home_card.dart';
+import 'package:smart_house_app/widgets/filter_section.dart';
 import 'package:smart_house_app/widgets/layouts/devices_layout.dart';
 
 class DevicePage extends StatelessWidget {
@@ -20,7 +24,27 @@ class DevicePage extends StatelessWidget {
         ),
         childWidget: Column(
           children: [
-
+            AuthInputField(
+                labelText: "Pesquisar...",
+                prefixIcon: CupertinoIcons.search,
+                keyboardType: TextInputType.text
+            ),
+            SizedBox(height: 8),
+            FilterSection(
+              filters: ["Tudo", "Iluminação", "Aquecimento", "Segurança"],
+              onSelected: (index) {
+                // TODO: atualizar lista de dispositivos
+              },
+            ),
+            SizedBox(height: 16),
+            Column(
+              children: [
+                DeviceHomeCard(deviceName: "Luz 1", icon: Icons.lightbulb),
+                DeviceHomeCard(deviceName: "Luz 2", icon: Icons.lightbulb),
+                DeviceHomeCard(deviceName: "Luz 3", icon: Icons.lightbulb),
+                DeviceHomeCard(deviceName: "Sensor gás", icon: Icons.sensors),
+              ],
+            )
           ],
         )
     );
